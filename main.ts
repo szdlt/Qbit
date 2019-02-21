c00010e552d6c7c6065b2cf68a96d63940ccd96c
szdlt <40625240+szdlt@users.noreply.github.com>
2019-02-21 09:44:16 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -998,9 +998,9 @@
      */
     //% weight=62 blockId=convertBattery block="Convert battery %data"
     export function convertBattery(str: string): string {
-      //  let cmdStr: string = "CMD|07|";
-	  let cmdStr: string = "";
-          cmdStr += str.substring(0,2);
+          let cmdStr: string = "CMD|07|";
+      //  let cmdStr: string = "";
+      //  cmdStr += str.substring(0,2);
       //  cmdStr += str.substring(0,1);
           cmdStr += "|$";
           return cmdStr;
